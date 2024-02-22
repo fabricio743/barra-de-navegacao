5b1e8cd1c1c002aef4235cf5bc66736a4de2b23a
hey-afro <heyafroo@gmail.com>
2024-02-22 06:06:52 -0300

    s2

---
+++ java/script.js
@@ -0,0 +1,9 @@
+function menuShow(){
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')){
+        else{
+            menuMobile.classList.add(open);
+            document.querySelector('icon').src"";
+        }
+    }
+}
