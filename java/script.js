50ea9a35fc9a413ad50510b82625053373470f58
hey-afro <heyafroo@gmail.com>
2024-02-25 05:53:58 -0300

    ATT3

--- java/script.js
+++ java/script.js
@@ -1,5 +1,6 @@
 function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
+    
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
         document.querySelector('.icon').src = "img/menu_white_36dp.svg";
